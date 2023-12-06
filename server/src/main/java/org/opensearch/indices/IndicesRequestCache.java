4bdf459338d0e87e8b0f6db1b96358e030906682
Peter Alfonsi <petealft@amazon.com>
2023-12-05 16:15:33 -0800

    added feature flag

--- server/src/main/java/org/opensearch/indices/IndicesRequestCache.java
+++ server/src/main/java/org/opensearch/indices/IndicesRequestCache.java
@@ -58,6 +58,7 @@ import org.opensearch.common.settings.Setting;
 import org.opensearch.common.settings.Setting.Property;
 import org.opensearch.common.settings.Settings;
 import org.opensearch.common.unit.TimeValue;
+import org.opensearch.common.util.FeatureFlags;
 import org.opensearch.common.util.concurrent.ConcurrentCollections;
 import org.opensearch.core.common.bytes.BytesReference;
 import org.opensearch.core.common.io.stream.StreamInput;
@@ -140,36 +141,35 @@ public final class IndicesRequestCache implements TieredCacheEventListener<Indic
                 throw new RuntimeException(e);
             }
         };
-        // enabling this for testing purposes. Remove/tweak!!
-        long CACHE_SIZE_IN_BYTES = 1000000L;
-        String SETTING_PREFIX = "indices.request.cache";
-        String STORAGE_PATH = indicesService.getNodePaths()[0].indicesPath.toString() + "/request_cache";
-
-        double diskTierKeystoreWeightFraction = 0.05; // Allocate 5% of the on-heap weight to the disk tier's keystore
-        long keystoreMaxWeight = (long) (diskTierKeystoreWeightFraction * INDICES_CACHE_QUERY_SIZE.get(settings).getBytes());
-
-        EhCacheDiskCachingTier<Key, BytesReference> ehcacheDiskTier = new EhCacheDiskCachingTier.Builder<Key, BytesReference>()
-            .setKeyType(Key.class)
-            .setValueType(BytesReference.class)
-            .setExpireAfterAccess(TimeValue.MAX_VALUE)
-            .setSettings(settings)
-            .setClusterSettings(clusterSettings)
-            .setThreadPoolAlias("ehcacheTest")
-            .setMaximumWeightInBytes(CACHE_SIZE_IN_BYTES)
-            .setStoragePath(STORAGE_PATH)
-            .setSettingPrefix(SETTING_PREFIX)
-            .setKeySerializer(new IRCKeyWriteableSerializer(this))
-            .setValueSerializer(new BytesReferenceSerializer())
-            .build();
 
         // Initialize tiered cache service. TODO: Enable Disk tier when tiered support is turned on.
-        tieredCacheService = new TieredCacheSpilloverStrategyService.Builder<Key, BytesReference>().setOnHeapCachingTier(
-            openSearchOnHeapCache
-        )
-            .setOnDiskCachingTier(ehcacheDiskTier)
-            .setTieredCacheEventListener(this)
-            .withPolicy(new DiskTierTookTimePolicy(settings, clusterSettings, transformationFunction))
-            .build();
+        TieredCacheSpilloverStrategyService.Builder<Key, BytesReference> tieredCacheServiceBuilder = new TieredCacheSpilloverStrategyService.Builder<Key, BytesReference>()
+            .setOnHeapCachingTier(openSearchOnHeapCache)
+            .setTieredCacheEventListener(this);
+
+        if (FeatureFlags.isEnabled(FeatureFlags.TIERED_CACHING)) {
+            // enabling this for testing purposes. Remove/tweak!!
+            long CACHE_SIZE_IN_BYTES = 1000000L;
+            String SETTING_PREFIX = "indices.request.cache";
+            String STORAGE_PATH = indicesService.getNodePaths()[0].indicesPath.toString() + "/request_cache";
+
+            EhCacheDiskCachingTier<Key, BytesReference> diskTier = new EhCacheDiskCachingTier.Builder<Key, BytesReference>()
+                .setKeyType(Key.class)
+                .setValueType(BytesReference.class)
+                .setExpireAfterAccess(TimeValue.MAX_VALUE)
+                .setSettings(settings)
+                .setClusterSettings(clusterSettings)
+                .setThreadPoolAlias("ehcacheTest")
+                .setMaximumWeightInBytes(CACHE_SIZE_IN_BYTES)
+                .setStoragePath(STORAGE_PATH)
+                .setSettingPrefix(SETTING_PREFIX)
+                .setKeySerializer(new IRCKeyWriteableSerializer(this))
+                .setValueSerializer(new BytesReferenceSerializer())
+                .build();
+
+            tieredCacheServiceBuilder.setOnDiskCachingTier(diskTier);
+        }
+        tieredCacheService = tieredCacheServiceBuilder.build();
         this.indicesService = indicesService;
     }
 
