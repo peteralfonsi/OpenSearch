a61f033201c694614a9471be97c4fa52eeb829bb
Peter Alfonsi <petealft@amazon.com>
2024-02-29 13:19:21 -0800

    Fixed on heap stats integration, added tests
    Signed-off-by: Peter Alfonsi <petealft@amazon.com>

--- server/src/main/java/org/opensearch/common/cache/store/OpenSearchOnHeapCache.java
+++ server/src/main/java/org/opensearch/common/cache/store/OpenSearchOnHeapCache.java
@@ -15,9 +15,9 @@ import org.opensearch.common.cache.ICache;
 import org.opensearch.common.cache.LoadAwareCacheLoader;
 import org.opensearch.common.cache.RemovalListener;
 import org.opensearch.common.cache.RemovalNotification;
+import org.opensearch.common.cache.RemovalReason;
 import org.opensearch.common.cache.stats.CacheStats;
 import org.opensearch.common.cache.ICacheKey;
-import org.opensearch.common.cache.stats.CacheStatsDimension;
 import org.opensearch.common.cache.stats.MultiDimensionCacheStats;
 import org.opensearch.common.cache.store.builders.ICacheBuilder;
 import org.opensearch.common.cache.store.config.CacheConfig;
@@ -31,6 +31,7 @@ import org.opensearch.common.settings.Setting;
 import org.opensearch.core.common.unit.ByteSizeValue;
 
 import java.util.Map;
+import java.util.function.ToLongBiFunction;
 
 import static org.opensearch.common.cache.store.settings.OpenSearchOnHeapCacheSettings.MAXIMUM_SIZE_IN_BYTES_KEY;
 
@@ -43,8 +44,9 @@ import static org.opensearch.common.cache.store.settings.OpenSearchOnHeapCacheSe
  */
 public class OpenSearchOnHeapCache<K, V> implements ICache<K, V>, RemovalListener<ICacheKey<K>, V> {
     private final Cache<ICacheKey<K>, V> cache;
-    private final CacheStats stats;
+    private CacheStats stats;
     private final RemovalListener<ICacheKey<K>, V> removalListener;
+    private final List<String> dimensionNames;
     public static final String TIER_DIMENSION_VALUE = "on_heap";
 
     public OpenSearchOnHeapCache(Builder<K, V> builder) {
@@ -56,7 +58,7 @@ public class OpenSearchOnHeapCache<K, V> implements ICache<K, V>, RemovalListene
             cacheBuilder.setExpireAfterAccess(builder.getExpireAfterAcess());
         }
         cache = cacheBuilder.build();
-        List<String> dimensionNames = Objects.requireNonNull(builder.dimensionNames, "Dimension names can't be null");
+        this.dimensionNames = Objects.requireNonNull(builder.dimensionNames, "Dimension names can't be null");
         this.stats = new MultiDimensionCacheStats(dimensionNames, TIER_DIMENSION_VALUE);
         this.removalListener = builder.getRemovalListener();
     }
@@ -76,6 +78,7 @@ public class OpenSearchOnHeapCache<K, V> implements ICache<K, V>, RemovalListene
     public void put(ICacheKey<K> key, V value) {
         cache.put(key, value);
         stats.incrementEntriesByDimensions(key.dimensions);
+        stats.incrementMemorySizeByDimensions(key.dimensions, cache.getWeigher().applyAsLong(key, value));
     }
 
     @Override
@@ -86,6 +89,7 @@ public class OpenSearchOnHeapCache<K, V> implements ICache<K, V>, RemovalListene
         } else {
             stats.incrementMissesByDimensions(key.dimensions);
             stats.incrementEntriesByDimensions(key.dimensions);
+            stats.incrementMemorySizeByDimensions(key.dimensions, cache.getWeigher().applyAsLong(key, value));
         }
         return value;
     }
@@ -93,12 +97,12 @@ public class OpenSearchOnHeapCache<K, V> implements ICache<K, V>, RemovalListene
     @Override
     public void invalidate(ICacheKey<K> key) {
         cache.invalidate(key);
-        stats.decrementEntriesByDimensions(key.dimensions);
     }
 
     @Override
     public void invalidateAll() {
         cache.invalidateAll();
+        stats = new MultiDimensionCacheStats(dimensionNames, TIER_DIMENSION_VALUE);
     }
 
     @Override
@@ -127,6 +131,13 @@ public class OpenSearchOnHeapCache<K, V> implements ICache<K, V>, RemovalListene
     @Override
     public void onRemoval(RemovalNotification<ICacheKey<K>, V> notification) {
         removalListener.onRemoval(notification);
+        stats.decrementEntriesByDimensions(notification.getKey().dimensions);
+        stats.incrementMemorySizeByDimensions(notification.getKey().dimensions, -cache.getWeigher().applyAsLong(notification.getKey(), notification.getValue()));
+
+        if (RemovalReason.EVICTED.equals(notification.getRemovalReason())
+            || RemovalReason.CAPACITY.equals(notification.getRemovalReason())) {
+            stats.incrementEvictionsByDimensions(notification.getKey().dimensions);
+        }
     }
 
     /**
