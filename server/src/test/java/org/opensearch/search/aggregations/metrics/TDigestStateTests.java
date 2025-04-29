f66f9585e59744356066d1daa9c2fa1841d71f0f
Peter Alfonsi <petealft@amazon.com>
2025-04-29 14:15:48 -0700

    add percentile error test with and without offset
    Signed-off-by: Peter Alfonsi <petealft@amazon.com>

--- server/src/test/java/org/opensearch/search/aggregations/metrics/TDigestStateTests.java
+++ server/src/test/java/org/opensearch/search/aggregations/metrics/TDigestStateTests.java
@@ -34,7 +34,13 @@ package org.opensearch.search.aggregations.metrics;
 
 import org.opensearch.test.OpenSearchTestCase;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import static org.opensearch.search.aggregations.metrics.PercentilesConfig.TDigest.DEFAULT_COMPRESSION;
 
 public class TDigestStateTests extends OpenSearchTestCase {
 
@@ -61,6 +67,47 @@ public class TDigestStateTests extends OpenSearchTestCase {
             assertTrue("Unexpectedly low value: " + v, v >= 0.0);
             assertTrue("Unexpectedly high value: " + v, v <= 1.0);
             prev = v;
+        }
+    }
+
+    public void testError() {
+        // Check if offsetting the values fed into the digest - for example as in a timestamp - affects accuracy.
+        //long offset = 0; //1_000_000_000_000L;
+        int numDocs = 100_000_000;
+        int rangeOfVariation = 10_000_000;
+        int numIters = 10;
+
+
+        List<Double> values = new ArrayList<>();
+        for (int i = 0; i < numDocs; i++) {
+            values.add(( double) i * rangeOfVariation / numDocs);
+        }
+        Collections.shuffle(values);
+
+        // Get errors without offset
+        System.out.println("truePercentile,percentileApproximation,abs(error)");
+        List<Long> elapsedTimes = new ArrayList<>();
+        for (long offset : new long[]{0, 1_000_000_000_000L}) {
+            for (int iter = 0; iter < numIters; iter++) {
+                TDigestState state = new TDigestState(DEFAULT_COMPRESSION);
+                long nanosStart = System.nanoTime();
+                for (double value : values) {
+                    state.add(value + offset);
+                }
+                long elapsed = System.nanoTime() - nanosStart;
+                elapsedTimes.add(elapsed);
+                for (double quantile = 0.01; quantile < 1.0; quantile += 0.01) {
+                    double percentileApproximation = state.quantile(quantile);
+                    double truePercentile = quantile * rangeOfVariation + offset;
+                    double error = Math.abs(percentileApproximation - truePercentile);
+                    String formatted = String.format(Locale.US, "%f,%f,%f", truePercentile, percentileApproximation, error);
+                    System.out.println(formatted);
+                }
+            }
+        }
+        System.out.println("\n\nElapsed times adding doc values for each run:");
+        for (long time : elapsedTimes) {
+            System.out.println(time);
         }
     }
 }
