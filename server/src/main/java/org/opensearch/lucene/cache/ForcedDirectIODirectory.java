5a3c680a5c24fbcc9064e257a03e3c471a84474e
Peter Alfonsi <petealft@amazon.com>
2025-12-16 14:45:58 -0800

    apply saurabh's changes
    Signed-off-by: Peter Alfonsi <petealft@amazon.com>

---
+++ server/src/main/java/org/opensearch/lucene/cache/ForcedDirectIODirectory.java
@@ -0,0 +1,41 @@
+package org.opensearch.lucene.cache;
+
+import org.apache.lucene.misc.store.DirectIODirectory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.IOContext;
+import org.apache.lucene.store.IndexOutput;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.OptionalLong;
+
+/**
+ * Directory impl which always uses IO for testing purposes.
+ */
+public class ForcedDirectIODirectory extends DirectIODirectory {
+
+    int blockSize;
+    private long minBytesDirect = 10 * 1024 * 1024;
+
+    public ForcedDirectIODirectory(FSDirectory delegate, int mergeBufferSize, long minBytesDirect) throws IOException {
+        super(delegate, mergeBufferSize, minBytesDirect);
+        this.blockSize = Math.toIntExact(Files.getFileStore(delegate.getDirectory()).getBlockSize());
+        System.out.println("====Block size returned from file system is ===" + blockSize);
+    }
+
+    public ForcedDirectIODirectory(FSDirectory delegate) throws IOException {
+        super(delegate);
+        this.blockSize = Math.toIntExact(Files.getFileStore(delegate.getDirectory()).getBlockSize());
+        System.out.println("====Block size returned from file system is ===" + blockSize);
+    }
+
+    protected boolean useDirectIO(String name, IOContext context, OptionalLong fileLength) {
+        return true;
+    }
+
+    @Override
+    public IndexOutput createOutput(String name, IOContext context) throws IOException {
+        ensureOpen();
+        return in.createOutput(name, context);
+    }
+}
