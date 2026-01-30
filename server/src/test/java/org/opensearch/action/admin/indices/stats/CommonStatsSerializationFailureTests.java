e02dea676552d90aa092a3e0f0a8589fae7a5530
Peter Alfonsi <petealft@amazon.com>
2026-01-30 10:27:44 -0800

    cleanup
    Signed-off-by: Peter Alfonsi <petealft@amazon.com>

--- server/src/test/java/org/opensearch/action/admin/indices/stats/CommonStatsSerializationFailureTests.java
+++ server/src/test/java/org/opensearch/action/admin/indices/stats/CommonStatsSerializationFailureTests.java
@@ -1,7 +1,8 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
  *
- * The OpenSearch Contributors require contributions made to\n * this file be licensed under the Apache-2.0 license or a
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
  * compatible open source license.
  */
 
